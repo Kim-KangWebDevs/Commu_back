637f56812757ab8c9cc7a2f1caa6bc439523e0df
dkdmlk <kang_in_seok>
2024-07-01 15:43:15 +0900

    first commit

---
+++ src/main/java/com/Commu_back/CommuBackApplication.java
@@ -0,0 +1,13 @@
+package com.Commu_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommuBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommuBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Commu_back/CommuBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Commu_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommuBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
