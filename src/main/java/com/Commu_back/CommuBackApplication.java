38dfaf249e7bada5750677a79b02d5c5c388de40
kimtaeu.dev <kimtaeu.dev@gmail.com>
2024-07-12 04:53:17 +0900

    스프링 시큐리티 로그인창 안뜨는 설정 추가

--- src/main/java/com/Commu_back/CommuBackApplication.java
+++ src/main/java/com/Commu_back/CommuBackApplication.java
@@ -2,8 +2,9 @@ package com.Commu_back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class CommuBackApplication {
 
 	public static void main(String[] args) {
@@ -11,4 +12,3 @@ public class CommuBackApplication {
 	}
 
 }
-//@ testing
