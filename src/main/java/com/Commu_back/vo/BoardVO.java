a739adbabc659e5ea965dcfe4ef9f8528dee1984
kimtaeu.dev <kimtaeu.dev@gmail.com>
2024-07-14 18:10:49 +0900

    BoardVO 수정

--- src/main/java/com/Commu_back/vo/BoardVO.java
+++ src/main/java/com/Commu_back/vo/BoardVO.java
@@ -18,20 +18,5 @@ public class BoardVO {
     private int board_bad;
     private String board_regdate; 
     private String board_updatedate; 
-    
-    public BoardVO() {
-    	
-    }
-    
-	public BoardVO(int board_no, String board_id, int user_no, String board_title, String board_content){
-		
-		super();
-		
-		this.board_no = board_no;
-		this.board_id = board_id;
-		this.user_no = user_no;
-		this.board_title = board_title;
-		this.board_content = board_content;
-		
-	}
+
 }
